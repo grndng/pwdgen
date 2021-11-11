1147f314f88fc809040f31f7c6396b8fc11b0055
grndng <gueren.dinga@hcu-hamburg.de>
2021-11-11 11:34:00 +0100

    Strong passwords should work now.

--- generator.py
+++ generator.py
@@ -1,4 +1,4 @@
-from random import randint, sample
+from random import randint, sample, shuffle
 import string
 
 alphabet = string.ascii_letters
@@ -6,14 +6,46 @@ digits = string.digits
 symbols = string.punctuation
 no_whitespace = string.printable.strip()
 
+
 def pwdgen(strength: str) -> str:
     strength = strength.lower()
-    pwd = ""
+
+    pwd = "00"
+
     if strength == "strong":
-        return "1a2!s3d4f"
-    
-    
+        while len(set(pwd.lower())) != len(pwd.lower()):
+            pwd = mandatory_strong()
+            pwd += "".join(sample(no_whitespace, randint(4, 8)))
+        pwd = list(pwd)
+        shuffle(pwd)
+        print(f'Returning {"".join(pwd)}')
+        return "".join(pwd)
+
     elif strength == "weak":
+
         return "a5afah"
 
-print(any(symbol in pwdgen("Strong") for symbol in symbols))
+
+def mandatory_strong() -> str:
+    """Small utility to:
+        - reset the password in case there are duplicates caught by the length-comparison in the while loop of pwdgen()
+        - fill up the string with the mandatory elements set for strong passwords (symbol, upper- and lowercase letter, digit)
+
+    Parameters:
+            None
+
+    Returns:
+            pwd (str): Returns
+
+    """
+    #TODO: concatenate string in a simpler fashion
+
+    pwd = ""
+    pwd += "".join(sample(alphabet[:26], 1))
+    pwd += "".join(sample(alphabet[26:], 1))
+    pwd += "".join(sample(digits, 1))
+    pwd += "".join(sample(symbols, 1))
+    return pwd
+
+
+print(mandatory_strong.__doc__)
--- generator_test.py
+++ generator_test.py
@@ -10,6 +10,7 @@ import string
 
 def test_pwdgen_strong_length():
     """Test if the length of strong passwords match"""
+    print(pwdgen("strong"))
     assert 8 <= len(pwdgen("strong")) <= 12
 
 
@@ -20,33 +21,29 @@ def test_pwdgen_weak_length():
 
 def test_pwdgen_strong_consecutive_lower():
     """Test if there are consecutive or duplicate symbols in strong passwords"""
-    print(pwdgen("strong"))
-    assert [
-        0
-        for i, j in enumerate(sorted(pwdgen("strong").lower())[:-1])
-        if j == sorted(pwdgen("strong").lower())[i + 1]
-    ] != [0]
+    pwd = "".join(sorted(pwdgen("strong"))).lower()
+    assert [0 for i, j in enumerate(pwd[:-1]) if pwd[i] == pwd[i + 1]] != [0]
 
 
 def test_pwdgen_weak_consecutive_lower():
     """Test if there are consecutive symbols in weak passwords"""
-    assert [
-        0
-        for i, j in enumerate(pwdgen("weak").lower()[:-1])
-        if j == pwdgen("weak").lower()[i + 1]
-    ] != [0]
+    pwd = pwdgen("weak").lower()
+    assert [0 for i, j in enumerate(pwd[:-1]) if j == pwd[i + 1]] != [0]
 
 
 def test_pwdgen_strong_symbols_duplicates():
     """Test if there are symbols in strong passwords"""
-    assert any(symbol in pwdgen("strong") for symbol in string.punctuation) == True
-    # any() is probably faster than iterating around
+    pwd = pwdgen("strong")
+    assert any(symbol in pwd for symbol in string.punctuation) == True
+    # any() is probably faster than iterating around:
     # assert len([i for i in pwdgen("strong") if i in string.punctuation]) != 0
 
 
 def test_pwdgen_weak_symbols():
     """Test if there are symbols in weak passwords"""
-    assert any(symbol in pwdgen("weak") for symbol in string.punctuation) == False
+    pwd = pwdgen("weak")
+    assert any(symbol in pwd for symbol in string.punctuation) == False
+    # using any instead of iterating over list:
     # assert len([i for i in pwdgen("weak") if i not in string.punctuation]) == len(
     #    pwdgen("weak")
     # )
